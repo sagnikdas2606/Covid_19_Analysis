d68abcb2529fae2e44852d989cfcac183271e54b
unknown <sagnikdas2606@gmail.com>
2026-01-30 11:16:23 +0530

    Added comments to convert_excel_to_csv.py

--- convert_excel_to_csv.py
+++ convert_excel_to_csv.py
@@ -4,9 +4,11 @@ import pandas as pd
 sheets = pd.read_excel("C:/Users/SAGNIK/Downloads/Covid_19_Analysis/Covid_19_Analysis/data/raw/covid_19_dataset-1718175635.xlsx", sheet_name=None)
 
 # Merge all sheets into one dataframe
+# The ignore index parameter is set to True to create a new sequential index
 merged_df = pd.concat(sheets.values(), ignore_index=True)
 
 # Save as CSV
+# The index parameter is set to False to exclude Dataframe index from CSV output
 merged_df.to_csv("C:/Users/SAGNIK/Downloads/Covid_19_Analysis/Covid_19_Analysis/data/processed/merged_data.csv", index=False)
 
 print("CSV created successfully!")
